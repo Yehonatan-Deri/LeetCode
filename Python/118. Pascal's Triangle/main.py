206e0b33a514ec3302d2f38e7356d7a1231a61a6
Yehonatan Deri <yonatander@gmail.com>
2024-09-18 18:47:13 +0300

    added another way for question 118

--- Python/118. Pascal's Triangle/main.py
+++ Python/118. Pascal's Triangle/main.py
@@ -11,6 +11,17 @@ def generate(numRows: int) -> List[List[int]]:
     return res
 
 
+def generate2(numRows: int) -> List[List[int]]:
+    res = [[1]]
+
+    for i in range(numRows - 1):
+        temp_row = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp_row[j] + temp_row[j + 1])
+        res.append(row)
+
+    return res
 
 if __name__ == '__main__':
     print(generate(5))
