6afdf0de6e22942f2cdba97de911be3ef1fb6e60
Yehonatan Deri <yonatander@gmail.com>
2025-09-25 14:55:06 +0300

    Easy - 88. Merge Sorted Array
    another implementation

--- Python/Easy - 88. Merge Sorted Array.py
+++ Python/Easy - 88. Merge Sorted Array.py
@@ -2,25 +2,47 @@ from typing import List
 
 
 class Solution:
+    # def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    #     """
+    #     Do not return anything, modify nums1 in-place instead.
+    #     """
+    #     end_pointer = m + n - 1
+    #
+    #     while m > 0 and n > 0:
+    #         if nums1[m - 1] > nums2[n - 1]:
+    #             nums1[end_pointer] = nums1[m - 1]
+    #             m -= 1
+    #         elif nums1[m - 1] <= nums2[n - 1]:
+    #             nums1[end_pointer] = nums2[n - 1]
+    #             n -= 1
+    #         end_pointer -= 1
+    #
+    #     while n > 0:
+    #         nums1[end_pointer] = nums2[n - 1]
+    #         n -= 1
+    #         end_pointer -= 1
+
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        end_pointer = m + n - 1
+        end = m + n - 1
 
-        while m > 0 and n > 0:
-            if nums1[m - 1] > nums2[n - 1]:
-                nums1[end_pointer] = nums1[m - 1]
-                m -= 1
-            elif nums1[m - 1] <= nums2[n - 1]:
-                nums1[end_pointer] = nums2[n - 1]
+        while end >= 0:
+            if m - 1 < 0:
+                nums1[end] = nums2[n - 1]
                 n -= 1
-            end_pointer -= 1
-
-        while n > 0:
-            nums1[end_pointer] = nums2[n - 1]
-            n -= 1
-            end_pointer -= 1
+            elif n - 1 < 0:
+                nums1[end] = nums1[m - 1]
+                m -= 1
+            else:
+                if nums1[m - 1] > nums2[n - 1]:
+                    nums1[end] = nums1[m - 1]
+                    m -= 1
+                else:
+                    nums1[end] = nums2[n - 1]
+                    n -= 1
+            end -= 1
 
 
 # Test cases
